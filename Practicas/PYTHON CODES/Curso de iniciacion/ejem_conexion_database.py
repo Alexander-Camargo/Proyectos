230228457121dc5c87ef3f40be4dcc37a2479b69
Alexander Camargo <alexanderi.camargoa@gmail.com>
2025-01-22 01:53:36 -0500

    apuntes para sentencias de SQL

--- Practicas/PYTHON CODES/Curso de iniciacion/ejem_conexion_database.py
+++ Practicas/PYTHON CODES/Curso de iniciacion/ejem_conexion_database.py
@@ -7,6 +7,26 @@ mydb = mysql.connector.connect(
 
 cursor = mydb.cursor()
 
-#sql = "CREATE DATABASE IF NOT EXISTS db_prueba"
+# sql = "CREATE DATABASE IF NOT EXISTS db_prueba"
+# sql = "INSERT INTO db_prueba (nombre, apellido, edad) VALUES (%s, %s, %s)"
+# val = ("valor1", "valor2", "valor3")
+
+# para insertar estas sentencias de SQL utilizamos:
+# cursor.execute(sql, val)
+# mydb.commit()
+# pimero le decimos que se ejecute y luego le damos la sentencia a ejecutar y los valores si es que lleva esta sentencia
+
+# verificar si el registro fue exitoso con:
+# print(cursor.rowcount, "record inserted.")
+
+# algunas sentencias pueden ser ejecutadas directamente por ejemplo:
+# cursor.execute("SElECT * FROM nombre_de_la_tabla")
+
+# buscar todos los registros:
+# resultado = cursor.fetchall()
+
+# imprimir todos los registros:
+# for x  in resultado:
+#    print(x)
 
 print(mydb)
